bc90ce79390d367541b2b1cfb6ba6285f0566f47
ketevanglonti <qetighlonti97@gmail.com>
2025-07-04 18:45:20 +0400

    js hw

---
+++ HW1.js
@@ -0,0 +1,33 @@
+var a=87
+var b=65
+console.log(a+b) ;
+
+let student = {firstName:"ketevan" , lastName:"glonti", age:28, email:"ketevan.glonti@example.com", marks:[87, 65] }
+console.log(student.firstName) ;
+
+let m=45
+let n=23
+if (m > n ) { console.log (m+n) }
+else {console.log(n) } ;
+
+let studentName = "ketevan glonti"
+console.log ("Rise and shine," + studentName + "!") ;
+
+let salary = 2500
+if (salary >= 2000) { bonus = 0 }
+else { bonus = salary * 0.1 }
+console.log(bonus) ;
+
+let a=23
+let b= a/2
+let c= a+b
+console.log (c) ;
+
+let phone = ["iphone", "samsung","pixel" ,"Huawei", "Xiaomi","Xiaomi"]
+console.log (phone) ;
+
+
+
+
+
+
